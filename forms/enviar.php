897ee24ab496cac1c4584f78d4a46e0a8d9d1f21
Ivan Molina <ivanmolina052@gmail.com>
2020-12-24 10:15:59 -0300

    envio de mail

---
+++ forms/enviar.php
@@ -0,0 +1,24 @@
+<?php  
+
+// Llamando a los campos
+$nombre = $_POST['nombre'];
+$correo = $_POST['correo'];
+$asunto = $_POST['asunto'];
+$mensaje = $_POST['mensaje'];
+
+// Datos para el correo
+$destinatario = "ivanmolina052@gmail.com";
+$asunto = "Enviado desde sitio web";
+
+$carta = "De: $nombre \n";
+$carta .= "Correo: $correo \n";
+$carta .= "Asunto: $asunto \n";
+$carta .= "Mensaje: $mensaje";
+
+// Enviando Mensaje
+$mail = mail($destinatario, $asunto, $carta);
+if($mail){
+    echo "<h4>Mensaje enviado con exito</4>";
+}
+
+?>
